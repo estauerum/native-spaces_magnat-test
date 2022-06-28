2e3363abcec6379c2f70b62e7c233238a3306e62
Auurora <iodko_dasha@bk.ru>
2022-06-28 14:58:10 +0300

    page-up

--- js/script.js
+++ js/script.js
@@ -51,3 +51,18 @@ $('.choose__slider').slick({
     arrows: false,
     focusOnSelect: true
   });
+
+//pageup
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
